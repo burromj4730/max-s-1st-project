1f3f4401e50e91d6fd2aad4ef11bd461d283fe4d
burromj4730 <maxburrows1827@gmail.com>
2023-05-18 19:31:24 +0930

    (almost) completed jump orbs

--- max's 1st project/Assets/scripts/OrbJump.cs
+++ max's 1st project/Assets/scripts/OrbJump.cs
@@ -6,16 +6,15 @@ public class OrbJump : MonoBehaviour
 {
     public float bounceForce;
 
+    public AnimationClip hit;
+
+    private float hitLength;
+
+    private Animator anim;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Vector2 vel = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
-            vel.y = 0;
-            collision.gameObject.GetComponent<Rigidbody2D>().velocity = vel;
-
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceForce);
-        }
+        
 
        
     }
@@ -28,7 +27,33 @@ public class OrbJump : MonoBehaviour
             collision.gameObject.GetComponent<Rigidbody2D>().velocity = vel;
 
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceForce);
+
+            anim.SetBool("Animation Done", false);
+            anim.SetBool("Hit", true);
         }
 
     }
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        hitLength = hit.length;
+    }
+
+    private void Update()
+    {
+        if (anim.GetBool("Hit"))
+        {
+            StartCoroutine("Timer");
+           
+        }
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(hitLength);
+        anim.SetBool("Hit", false);
+        anim.SetBool("Animation Done", true);
+    }
+
 }
