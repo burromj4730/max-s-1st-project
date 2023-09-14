8763a9e5699ddcfc61823d02f219bcacf3061d21
burromj4730 <maxburrows1827@gmail.com>
2023-09-14 19:27:19 +0930

    Almost done with the spike death

--- max's 1st project/Assets/scripts/Spiked.cs
+++ max's 1st project/Assets/scripts/Spiked.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Spiked : MonoBehaviour
 {
     public Sprite spiked;
+
+    public GameObject deathParticals;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
@@ -13,6 +15,39 @@ public class Spiked : MonoBehaviour
             collision.gameObject.GetComponent<Animator>().enabled = false;
             collision.gameObject.GetComponent<SpriteRenderer>().sprite = spiked;
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-2, 2), 15) * 50f);
+            StartCoroutine(ExplodePlayer(collision.gameObject));
         }
     }
+    IEnumerator ExplodePlayer(GameObject player)
+    {
+        yield return new WaitForSeconds(0.25f);
+        StartCoroutine(fadeToWhite(player));
+        yield return new WaitForSeconds(0.25f);
+        player.GetComponent<Rigidbody2D>().gravityScale = 0;
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        Instantiate(deathParticals, player.transform);
+        StartCoroutine(TurnOfSprite(player));
+    }
+    IEnumerator TurnOfSprite(GameObject player)
+    {
+        yield return new WaitForSeconds(0.1f);
+        player.GetComponent<SpriteRenderer>().enabled = false;
+        player.transform.GetChild(2).GetComponent<SpriteRenderer>().enabled = false;
+    }
+    IEnumerator fadeToWhite(GameObject player)
+    {
+        Color transp = player.transform.GetChild(2).GetComponent<SpriteRenderer>().color;
+        yield return new WaitForSeconds(0.025f);
+        transp.a += 0.1f;
+        player.transform.GetChild(2).GetComponent<SpriteRenderer>().color = transp;
+        if (player.transform.GetChild(2).GetComponent<SpriteRenderer>().color.a !=1)
+        {
+            StartCoroutine(fadeToWhite(player));
+        }
+    }
+    IEnumerable screenShake(GameObject player)
+    {
+        yield return new 
+    }
 }
+
