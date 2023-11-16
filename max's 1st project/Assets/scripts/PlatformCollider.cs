80ee33cfaced9e5ea228c143d664201bb18d72dc
burromj4730 <maxburrows1827@gmail.com>
2023-11-16 19:26:17 +1030

    Added platforms | made falling(ofacliff) animations

---
+++ max's 1st project/Assets/scripts/PlatformCollider.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformCollider : MonoBehaviour
+{
+    public Collider2D M_collider;
+
+    public Rigidbody2D player;
+
+    [Range(0f, 5f)] public float floating_Movement;
+
+    [Range(0f, 0.1f)] public float floating_Speed;
+
+    private bool moving_Up = false;
+
+    private float starting_Y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        starting_Y = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.velocity.y > 0)
+        {
+            M_collider.enabled = false;
+        }
+        else
+        {
+            M_collider.enabled = true;
+        }
+        if (moving_Up)
+        {
+            if (transform.position.y < starting_Y + floating_Movement)
+            {
+                transform.position += new Vector3(0, floating_Speed, 0);
+            }
+            else
+            {
+                moving_Up = false;
+            }
+        }
+        else
+        {
+            if (transform.position.y > starting_Y - floating_Movement)
+            {
+                transform.position -= new Vector3(0, floating_Speed, 0);
+            }
+            else
+            {
+                moving_Up = true;
+            }
+        }
+    }
+}
