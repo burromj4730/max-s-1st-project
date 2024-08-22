e38af9d6c85e6f2c634b5f55f1591298cd427186
burromj4730 <maxburrows1827@gmail.com>
2024-08-22 19:21:32 +0930

    added camera mechanics

--- Assets/scripts/CameraFollowPlayer.cs
+++ Assets/scripts/CameraFollowPlayer.cs
@@ -8,8 +8,21 @@ public class CameraFollowPlayer : MonoBehaviour
     public Transform tofollow;
 
     private bool justCollided;
+//-------------------------------------------------------------
 
-    // Update is called once per frame
+    [System.Serializable]
+    public struct cameraFreeze
+    {
+        public float startFreezeX;
+        public Vector2 freezePositionMin;
+        public Vector2 freezePositionMax;
+        public float endFreezeX;
+        public bool islast;
+    }
+
+    public List<cameraFreeze> FreezePosition = new List<cameraFreeze>();
+    public int currentPosition = 0;
+
     void Update()
     {
         if (!justCollided)
@@ -26,6 +39,13 @@ public class CameraFollowPlayer : MonoBehaviour
                 transform.position = new Vector3(transform.position.x, tofollow.position.y, transform.position.z);
             }
         }
+        if (tofollow.position.x > FreezePosition[currentPosition].startFreezeX)
+        {
+            if(tofollow.position.x< FreezePosition[currentPosition].endFreezeX)
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(((FreezePosition[currentPosition].freezePositionMax.x + FreezePosition[currentPosition].freezePositionMin.x)/2)-tofollow.position.x, ((FreezePosition[currentPosition].freezePositionMax.y + FreezePosition[currentPosition].freezePositionMin.y)/2)-tofollow.position.y,transform.position.z),1f);
+            }
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -37,4 +57,5 @@ public class CameraFollowPlayer : MonoBehaviour
         yield return new WaitForSeconds(0.1f);
         justCollided = false;
     }
+
 }
