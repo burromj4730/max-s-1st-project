f4c0592db8924217975a0d34b89b54673ee1bf3f
burromj4730 <maxburrows1827@gmail.com>
2024-09-19 18:23:17 +0930

    a

--- Assets/scripts/CameraFollowPlayer.cs
+++ Assets/scripts/CameraFollowPlayer.cs
@@ -30,6 +30,8 @@ public class CameraFollowPlayer : MonoBehaviour
     public List<cameraFreeze> FreezePosition = new List<cameraFreeze>();
     public int currentPosition = 0;
 
+    private bool bossCamOn = false;
+
     private void Start()
     {
         cam = GetComponent<Camera>();
@@ -51,38 +53,55 @@ public class CameraFollowPlayer : MonoBehaviour
                 transform.position = new Vector3(transform.position.x, tofollow.position.y, transform.position.z);
             }
         }
-        if (tofollow.position.x > FreezePosition[currentPosition].startFreezeX && !reachedRoomCentre) 
+        if(tofollow.position.x > FreezePosition[currentPosition].startFreezeX)
         {
-            tofollow.gameObject.GetComponent<playerMovement>().canMove = false;
-            tofollow.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
-            Vector3 playerPos = tofollow.position;
-            float newCamx = playerPos.x > FreezePosition[currentPosition].freezePosition.x ? Mathf.Abs(playerPos.x - FreezePosition[currentPosition].freezePosition.x) * -1 : Mathf.Abs(FreezePosition[currentPosition].freezePosition.x - playerPos.x);
-            newCamx *= 0.5f;
-            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(newCamx,(FreezePosition[currentPosition].freezePosition.y) - tofollow.position.y, transform.position.z), moveTime);
-            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, FreezePosition[currentPosition].cameraSize, moveTime);
-            Debug.Log("newX: " + newCamx + "\ntransformX: " + transform.localPosition.x);
-            if (Vector2.Distance(new Vector2(transform.localPosition.x,transform.position.y), new Vector2(newCamx, FreezePosition[currentPosition].freezePosition.y)) <= 0.1f)
+            bossCamOn = true;
+
+        }
+        else if(tofollow.position.x > FreezePosition[currentPosition].endFreezeX)
+        {
+            bossCamOn = false;
+        }
+        if (bossCamOn)
+        {
+            if (tofollow.position.x > FreezePosition[currentPosition].startFreezeX && !reachedRoomCentre) 
             {
-                reachedRoomCentre = true;
-                tofollow.gameObject.GetComponent<playerMovement>().canMove = true;
-                tofollow.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
-                tofollow.gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
+                tofollow.gameObject.GetComponent<playerMovement>().canMove = false;
+                tofollow.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+                Vector3 playerPos = tofollow.position;
+                float newCamx = playerPos.x > FreezePosition[currentPosition].freezePosition.x ? Mathf.Abs(playerPos.x - FreezePosition[currentPosition].freezePosition.x) * -1 : Mathf.Abs(FreezePosition[currentPosition].freezePosition.x - playerPos.x);
+                newCamx *= 0.5f;
+                transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(newCamx,(FreezePosition[currentPosition].freezePosition.y) - tofollow.position.y, transform.position.z), moveTime);
+                cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, FreezePosition[currentPosition].cameraSize, moveTime);
+                Debug.Log("newX: " + newCamx + "\ntransformX: " + transform.localPosition.x);
+                if (Vector2.Distance(new Vector2(transform.localPosition.x,transform.position.y), new Vector2(newCamx, FreezePosition[currentPosition].freezePosition.y)) <= 0.1f)
+                {
+                    reachedRoomCentre = true;
+                    tofollow.gameObject.GetComponent<playerMovement>().canMove = true;
+                    tofollow.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+                    tofollow.gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
+                }
+            }
+            else if(reachedRoomCentre)
+            {
+                Vector3 playerPos = tofollow.position;
+                float newCamx = playerPos.x > FreezePosition[currentPosition].freezePosition.x ? Mathf.Abs(playerPos.x - FreezePosition[currentPosition].freezePosition.x) *-1 : Mathf.Abs(FreezePosition[currentPosition].freezePosition.x - playerPos.x);
+                newCamx *= 0.5f;
+                playerPos.x = 0f; 
+                transform.localPosition = new Vector3(newCamx, FreezePosition[currentPosition].freezePosition.y, transform.localPosition.z)-playerPos;
+            
+            }
+            if (tofollow.position.x > FreezePosition[currentPosition].endFreezeX)
+            {
+                reachedRoomCentre = false;
+                if (currentPosition + 1 != FreezePosition.Count)
+                {
+                    currentPosition++;
+                }
+                
             }
         }
-        else if(reachedRoomCentre)
-        {
-            Vector3 playerPos = tofollow.position;
-            float newCamx = playerPos.x > FreezePosition[currentPosition].freezePosition.x ? Mathf.Abs(playerPos.x - FreezePosition[currentPosition].freezePosition.x) *-1 : Mathf.Abs(FreezePosition[currentPosition].freezePosition.x - playerPos.x);
-            newCamx *= 0.5f;
-            playerPos.x = 0f; 
-            transform.localPosition = new Vector3(newCamx, FreezePosition[currentPosition].freezePosition.y, transform.localPosition.z)-playerPos;
-            
-        }
-        if (tofollow.position.x > FreezePosition[currentPosition].endFreezeX)
-        {
-            reachedRoomCentre = false;
-            currentPosition++;
-        }
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
