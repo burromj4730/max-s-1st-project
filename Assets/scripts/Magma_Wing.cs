a192ee3c522c47741c4393dcc8208b4f7a09a177
burromj4730 <maxburrows1827@gmail.com>
2024-10-17 19:32:38 +1030

    a

--- Assets/scripts/Magma_Wing.cs
+++ Assets/scripts/Magma_Wing.cs
@@ -20,10 +20,34 @@ public class Magma_Wing : MonoBehaviour
 
     private bool playerInRadius;
 
+    public float timeBetweenAtacks;
+
+    public float freezePosMomento;
+
+    private bool attackCooldown;
+
+    private Transform player;
+
+    public GameObject fireBall;
+
+    private Rigidbody2D RB;
+
+    public float moveSpeed;
+
+    public float idealDistance;
+
+    public float acceleration;
+
+    public float fireBallMovespeed;
+
+    private bool attackStarted;
+
+    public float attackEndFrames;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        RB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -40,6 +64,10 @@ public class Magma_Wing : MonoBehaviour
                 magmaWing.SetBool("Idle", true);
                 magmaWing.SetBool("Alerted", false);
                 magmaWing.SetBool("Agro", false);
+                if (playerInRadius)
+                {
+                    Follow();
+                }
                 break;
             case state.ALERTED:
                 magmaWing.SetBool("Idle", false);
@@ -62,26 +90,91 @@ public class Magma_Wing : MonoBehaviour
         switch (currentState)
         {
             case state.IDLE:
-                RaycastHit2D[] circle = Physics2D.CircleCastAll(transform.position, idleRadius, Vector2.up, 0f, playerMask);
-
-                if (circle.Length != 0)
+                if (!playerInRadius)
                 {
-                    if (playerInRadius == false)
+                    RaycastHit2D[] circle = Physics2D.CircleCastAll(transform.position, idleRadius, Vector2.up, 0f, playerMask);
+                    if (circle.Length!=0)
                     {
                         currentState = state.ALERTED;
                         playerInRadius = true;
+                        player = circle[0].transform;
                     }
-                    
                 }
                 else
                 {
-                    playerInRadius = false;
+                    if (!attackCooldown)
+                    {
+                        currentState = state.AGRO;
+                    }
                 }
+                break;
+            case state.AGRO:
+                Attack();
                 break;
         }
     }
     void alertTimer()
     {
         currentState = state.IDLE;
+    }
+    void AGROTimer()
+    {
+        currentState = state.AGRO;
+        attackCooldown = false;
+    }
+    void Attack()
+    {
+        if (!attackStarted)
+        {
+            attackStarted = true;
+            Invoke("AttackFreese", freezePosMomento);
+        }
+    }
+    void AttackFreese()
+    {
+        RB.velocity = Vector2.zero;
+        Vector3 direction = (player.position - transform.position).normalized;
+        GameObject newFire = Instantiate(fireBall, transform);
+        newFire.GetComponent<Rigidbody2D>().velocity = direction * fireBallMovespeed;
+        Invoke("FinishAttack", attackEndFrames);
+    }
+    void FinishAttack()
+    {
+        currentState = state.IDLE;
+        attackCooldown = true;
+        Invoke("AttackCooldown", timeBetweenAtacks);
+    }
+    void AttackCooldown()
+    {
+        attackCooldown = false;
+    }
+    void Follow()
+    {
+        Vector3 direction = player.position - transform.position;
+        direction = direction.normalized;
+        if (Vector2.Distance(player.position, transform.position) < idealDistance)  
+        {
+            if (RB.velocity.magnitude < ((Vector2)direction * -moveSpeed).magnitude)
+            {
+                RB.velocity += (Vector2)direction * -acceleration;
+            }
+            else
+            {
+                RB.velocity = (Vector2)direction * -moveSpeed;
+            }
+            
+        }
+        else
+        {
+            if (RB.velocity.magnitude < ((Vector2)direction * moveSpeed).magnitude)
+            {
+                RB.velocity += (Vector2)direction * acceleration;
+            }
+            else
+            {
+                RB.velocity = (Vector2)direction * moveSpeed;
+            }
+        }
+       
     }
 }
